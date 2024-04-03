974c3a86d68d16ceaae03a065bbafa5f0ec26aaf
paulopariz <paulopariz01@gmail.com>
2024-04-03 20:39:33 -0300

    feat: checkin

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -12,7 +12,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
           attendeeId: z.coerce.number().int(),
         }),
 
-        response: {},
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
@@ -37,7 +46,17 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         throw new Error("Attendee not found.");
       }
 
-      return reply.send({ attendee });
+      const baseURL = `${request.protocol}://${request.hostname}`;
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL);
+
+      return reply.send({
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString(),
+        },
+      });
     }
   );
 }
