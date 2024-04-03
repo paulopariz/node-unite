95e0bc44f1936fdfbca0cc603bdd33b95a515441
paulopariz <paulopariz01@gmail.com>
2024-04-03 12:27:17 -0300

    init

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-evente";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
+app.register(registerForEvent);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Running");
+});
---
+++ src/utils/generate-slug.ts
@@ -0,0 +1,8 @@
+export function generateSLug(text: string): string {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-");
+}
