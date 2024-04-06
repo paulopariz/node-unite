43cbb54d9a7e9d40381a965c03a92c9c3b9fab4c
paulopariz <paulopariz01@gmail.com>
2024-04-05 21:34:07 -0300

    feat: swagger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -8,6 +15,25 @@ import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+
+    info: {
+      title: "pass-in",
+      description: "Documentação oficial da API do pass-in",
+      version: "1.0.0",
+    },
+  },
+
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
